74583217ee20429c1575d322c59c84d7cdfd41b2
Hasrox <hasnzn8@gmail.com>
2025-03-13 07:58:52 +0300

    Update index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -4,26 +4,27 @@ export const formatInput = (input) => {
 	return input.toLowerCase().replace(/\s+/g, '_');
 };
 
+// src/helpers/index.js - Updated formatContent function
 export const formatContent = (contentList, contentType) => {
 	return contentList.map((content) => {
-		if (contentType === 'VOD') {
-			const hours = convertTime(content.duration);
-			const masterUrl = content.source;
-			const url = masterUrl.replace(
-				/\/hls\/master\.m3u8$/,
-				'/hls/480p30/playlist.m3u8'
-			);
-			return {
-				name: `${content.session_title} - ${hours} - ${content.start_time}`,
-				value: url,
-				description: `- Description: ${content.session_title} | ${content.categories[0].name} | ${content.views} views`,
-			};
-		}
-
+	  if (contentType === 'VOD') {
+		const hours = convertTime(content.duration);
+		const masterUrl = content.source;
+		const url = masterUrl.replace(
+		  /\/hls\/master\.m3u8$/,
+		  '/hls/480p30/playlist.m3u8'
+		);
 		return {
-			name: `${content.title} - ${content.duration} seconds`,
-			value: content.clip_url,
-			description: `- Description: ${content.title} | ${content.category.name} | ${content.views} views`,
+		  name: `${content.session_title} - ${hours} - ${content.start_time}`,
+		  value: url,
+		  description: `- Description: ${content.session_title} | ${content.categories && content.categories[0] ? content.categories[0].name : 'No category'} | ${content.views || 0} views`,
 		};
+	  }
+  
+	  return {
+		name: `${content.title} - ${content.duration} seconds`,
+		value: content.clip_url,
+		description: `- Description: ${content.title} | ${content.category ? content.category.name : 'No category'} | ${content.views || 0} views`,
+	  };
 	});
-};
+  };
