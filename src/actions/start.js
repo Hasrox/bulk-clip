0e3b0c350d8109e3e24e1d6d6b74113a5d6667b0
Hasrox <hasnzn8@gmail.com>
2025-03-13 07:57:15 +0300

    Update start.js

--- src/actions/start.js
+++ src/actions/start.js
@@ -1,36 +1,25 @@
-import {
-	promptChannelName,
-	promptContent,
-	promptContentType,
-	promptDownload,
-} from '../utils/prompts.js';
-import API from '../api/index.js';
-import { formatContent } from '../helpers/index.js';
-import { Downloader } from '../lib/downloader.js';
-
-/*const handleExit = () => process.exit(0);
-process.on('SIGINT', () => handleExit());
-process.on('SIGTERM', () => handleExit());*/
-
-export const initialAction = async () => {
-	try {
-		const channel = await promptChannelName();
-		const infoChannel = await API.fetchChannel(channel);
-		const { username } = infoChannel.user;
-		const contentType = await promptContentType(username);
-		const contentList = await API.fetchContentList(channel, contentType);
-		const formattedContent = formatContent(contentList, contentType);
-		const content = await promptContent(formattedContent, contentType);
-		const confirmDownload = await promptDownload(contentType, username);
-		const statusDownload = await Downloader(confirmDownload, content);
-		console.log(statusDownload.message);
-	} catch (error) {
-		if (error.name === 'ExitPromptError') {
-			process.exit(0);
-		}
-
-		throw error;
-	} finally {
-		//console.log('Finished');
-	}
-};
+if (downloadType === 'bulk') {
+  logMessage('Fetching all clips, this may take a while...', 'blue');
+  const response = await API.fetchContentList(channel, contentType, true);
+  const allClips = response.clips || [];
+  
+  if (!allClips || allClips.length === 0) {
+    logMessage('No clips found for this channel.', 'red');
+    return;
+  }
+  
+  logMessage(`Found ${allClips.length} clips!`, 'green');
+  const formattedContent = formatContent(allClips, contentType);
+  const confirmDownload = await confirm({
+    message: `Ready to download all ${allClips.length} clips from ${username}?`,
+    default: true,
+    transformer: confirmTransformer,
+    theme: {
+      prefix: logMessage('[?]', 'green'),
+    },
+  });
+  
+  if (confirmDownload) {
+    // ... rest of the bulk download code
+  }
+}
